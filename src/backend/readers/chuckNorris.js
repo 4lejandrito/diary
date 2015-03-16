d922080389343b0516f423d55c1538d9a1d75418
Matthew Alner <matthew.alner@wds.co>
2015-03-16 18:33:25 +0000

    chuck and broken gmail

--- src/backend/readers/chuckNorris.js
+++ src/backend/readers/chuckNorris.js
@@ -1,37 +1,35 @@
 var CronJob = require('cron').CronJob;
+var rest = require('superagent');
 
 module.exports = {
-    type: 'ChuckNorrisJokes',
+    type: 'Chuck Norris Jokes',
     image: 'http://cdn.marketplaceimages.windowsphone.com/v8/images/9de9475b-92c7-4617-b27b-6d1025c0ff90?imageType=ws_icon_large',
     description: 'Get a random daily Chuck Norris joke',
     schema: {
     },
+    getChuckNorrisFact: function() {
+
+    },
     instance: function(user, emit) {
-        var interval;
+        var interval, dailyJob = new CronJob({
+          cronTime: '00 00 08 * * 1-7',
+          onTick: function() {
+            rest.get('http://api.icndb.com/jokes/random', function(response){
+              emit({
+                  value: response.body.value.joke
+              });
+            });
+          },
+          start: false
+        });
+
         return {
             start: function() {
-              var dailyJob = new CronJob({
-                //cronTime: '0 0 8 1/1 * ? *',
-                cronTime: '0 0/5 * 1/1 * ? *',
-                onTick: function() {
-                  emit({
-                      value: this.getChuckNorrisFact()
-                  });
-                },
-                start: false,
-                timeZone: "Europe/England"
-              });
               dailyJob.start();
             },
             stop: function() {
               dailyJob.stop();
             }
         };
-    },
-    getChuckNorrisFact: function() {
-      // $.get('http://api.icndb.com/jokes/random', function(response){
-      //   return response.value.joke;
-      // });
-      return "BLARG";
     }
 };
