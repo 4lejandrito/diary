b10c7f14fa3d3c37e43bd4df5a48e56fcfa6bf65
Alejandro Tardin <alejandro@tardin.com>
2015-03-29 16:37:40 +0100

    Added a random image in every random event

--- src/backend/readers/random.js
+++ src/backend/readers/random.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 module.exports = {
     type: 'random',
     image: 'https://www.random.org/analysis/randbitmap-rdo-section.png',
-    description: 'Tracks random events, pretty useless',
+    description: 'Generate random events, very useful for testing',
     schema: {
         interval: 500
     },
@@ -13,7 +13,13 @@ module.exports = {
             start: function() {
                 interval = setInterval(function() {
                     emit({
-                        date: moment().dayOfYear(Math.random() * 365).toDate(),
+                        date: moment()
+                                .dayOfYear(Math.random() * 365)
+                                .hour(Math.random() * 24)
+                                .minute(Math.random() * 60)
+                                .toDate(),
+                        image: 'http://lorempixel.com/400/200?r=' + Math.random(),
+                        url: 'http://lorempixel.com/400/200',
                         value: Math.random()
                     });
                 }, user.readers.random.interval);
