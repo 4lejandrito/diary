923587bf758d7476f5759ec6fd21410174195532
Alejandro Tardin <alejandro@tardin.com>
2015-04-15 16:59:25 +0200

    Fixed chuck norris reader

--- src/backend/readers/chuckNorris.js
+++ src/backend/readers/chuckNorris.js
@@ -5,22 +5,19 @@ module.exports = {
     type: 'Chuck Norris Jokes',
     image: 'http://s1.postimg.org/dhn0rp2un/9de9475b_92c7_4617_b27b_6d1025c0ff90.png',
     description: 'Get a random daily Chuck Norris joke',
-    schema: {
-    },
-    getChuckNorrisFact: function() {
-
-    },
+    schema: {},
     instance: function(emit) {
         var dailyJob = new CronJob({
-          cronTime: '00 00 08 * * 1-7',
-          onTick: function() {
-            rest.get('http://api.icndb.com/jokes/random', function(response){
-              emit({
-                  value: response.body.value.joke
-              });
-            });
-          },
-          start: false
+            cronTime: '00 00 08 * * 1-7',
+            onTick: function() {
+                rest.get('http://api.icndb.com/jokes/random?escape=javascript').end(function(err, response) {
+                    if (response && response.body) {
+                        emit({
+                            description: response.body.value.joke
+                        });
+                    }
+                });
+            }
         });
 
         return {
