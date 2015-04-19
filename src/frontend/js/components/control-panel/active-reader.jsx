64552f33cdcc7e7804f3d6bbbf2d3609559b078f
Alejandro Tardin <alejandro@tardin.com>
2015-04-20 00:02:01 +0200

    Some style changes

--- src/frontend/js/components/control-panel/active-reader.jsx
+++ src/frontend/js/components/control-panel/active-reader.jsx
@@ -8,22 +8,25 @@ module.exports = React.createClass({
     getInitialState: function() {
         return {disabled: false};
     },
-    remove: function() {
+    remove: function(event) {
+        event.stopPropagation();
+        event.preventDefault();
         api.removeReader(this.props.reader);
         this.setState({disabled: true});
     },
     render: function() {
+        var className = '';
+        if (this.props.reader.error) {
+            className += 'error';
+        }
         return <li className="active-reader">
-            <Link to="new" params={{type: this.props.reader.type}}>
+            <Link className={className} to="new" params={{type: this.props.reader.type}}>
                 <h4>{this.props.reader.type}</h4>
                 <img src={'/api/reader/' + this.props.reader.type + '/picture'}/>
-                {
-                    _.mapObject(this.props.reader.settings, function(value, key) {
-                        return <p><strong>{key}</strong>: {value}</p>;
-                    })
-                }
+                <button className="danger" onClick={this.remove}>
+                    <Icon name="close"/> Remove
+                </button>
             </Link>
-            <button onClick={this.remove}><Icon name="close"/></button>
         </li>;
     }
 });
