15049022b008a69400462735f38e655a1d4a60ea
Alejandro Tardin <alejandro@tardin.com>
2015-05-15 19:55:14 +0100

    Now reading the email address directly from google rather than asking it to the user

--- src/backend/readers/gmail.js
+++ src/backend/readers/gmail.js
@@ -6,16 +6,11 @@ module.exports = {
     image: 'http://upload.wikimedia.org/wikipedia/commons/thumb/4/45/New_Logo_Gmail.svg/1000px-New_Logo_Gmail.svg.png',
     description: 'Tracks your Gmail account',
     schema: {
-        address: {
-            type: 'text',
-            description: 'Your Gmail address',
-            example: 'me@gmail.com'
-        },
         oauth2: {
             provider: 'google',
             strategy: require('passport-google-oauth').OAuth2Strategy,
             params: {
-                scope: ['https://mail.google.com/', 'profile']
+                scope: ['https://mail.google.com/', 'profile', 'email']
             },
             authParams: {
                 approvalPrompt: 'force',
@@ -26,7 +21,8 @@ module.exports = {
     tick: function(reader) {
         return email.tick(extend(true, {
             settings: {
-                server: 'imap.gmail.com'
+                server: 'imap.gmail.com',
+                address: reader.profile.emails[0].value
             }
         }, reader));
     }
