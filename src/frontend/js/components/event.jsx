9afcf19c32c4fa3b9174c0718ce671ab6a1aceb4
Alejandro Tardin <alejandro@tardin.com>
2015-06-01 19:11:53 +0100

    Events are now rendered from the semantics object

--- src/frontend/js/components/event.jsx
+++ src/frontend/js/components/event.jsx
@@ -1,15 +1,141 @@
 var React = require('react');
-var eventComponents = require('./events/*', {hash: true});
+var Link = require('components/link');
+var Icon = require('components/icon');
+var Gravatar = require('react-gravatar');
+var _ = require('underscore');
+
+var Person = React.createClass({
+    render: function() {
+        var name = this.props.isYou ? 'You' : this.props.name;
+        return this.props.url ? <Link href={this.props.url}>
+            {name}
+        </Link> : <span>{name}</span>;
+    }
+});
+
+var WhatType = React.createClass({
+    render: function() {
+        if (this.props.type === 'person') return <Person {...this.props}/>;
+        var name = this.props.type;
+        if (this.props.name) name += ' ' + this.props.name;
+        return <span>
+            {this.props.article}{' '}
+            {this.props.url ? <Link href={this.props.url}>
+                {name}
+            </Link> : name}
+        </span>;
+    }
+});
+
+var WhatList = React.createClass({
+    render: function() {
+        return this.props.what.length > 1 ?
+        <ul>
+            {_.pairs(
+                _.countBy(this.props.what, function(w) {return w.type;})
+            ).map(function(pair) {
+                var type = pair[0], count = pair[1];
+                return <li>{count + ' ' + type + 's'}</li>;
+            })}
+        </ul> : <WhatType {...this.props.what[0]}/>;
+    }
+});
+
+var Complements = React.createClass({
+    render: function() {
+        return <ul>
+            {this.props.complements.map(function(c) {
+                return <li> {c.preposition} <WhatType {...c}/></li>;
+            })}
+        </ul>;
+    }
+});
 
 module.exports = React.createClass({
+    getIcon: function(semantics) {
+        switch(semantics.verb) {
+            case 'watched': return <Icon name="youtube-play"/>;
+            case 'added': return <Icon name="plus"/>;
+            case 'created': return <Icon name="plus"/>;
+            case 'deleted': return <Icon name="minus"/>;
+            case 'pushed': return <Icon name="upload"/>;
+            case 'published': return <Icon name="cloud-upload"/>;
+            case 'shared': return <Icon name="share-alt"/>;
+            case 'sent': return <Icon name="send"/>;
+        }
+    },
+    render_link: function(link) {
+        return <Link href={link.url} className="preview">
+            {link.picture && <img src={link.picture}/>}
+            <strong><small>{link.title}</small></strong>
+            <small>{link.source}</small>
+            <small>{link.text}</small>
+        </Link>;
+    },
+    render_picture: function(picture) {
+        return [
+            <strong><small>{picture.title}</small></strong>,
+            <small>{picture.text}</small>,
+            <Link href={picture.url}>
+                <div><img src={picture.picture}/></div>
+            </Link>
+        ];
+    },
+    render_video: function(video) {
+        return [
+            <small>{video.title}</small>,
+            <div className="video">
+                <img src={video.picture}/>
+                <Link href={video.url} className="play">
+                    <Icon name="youtube-play"/>
+                </Link>
+            </div>
+        ];
+    },
+    render_commit: function(commit) {
+        return [
+            <Gravatar email={commit.source} size={50}/>,
+            <Link href={commit.url}>
+                <small>{commit.text}</small>
+            </Link>
+        ];
+    },
+    render_what: function(what) {
+        var f = this['render_' + what.type];
+        if (f) return f(what);
+        return [
+            what.title && <small>{what.title}</small>,
+            what.text && <blockquote>{what.text}</blockquote>
+        ];
+    },
+    render_where: function(where) {
+        if (where) {
+            var mapsUrl = 'http://maps.google.com/?q=', name = where.name;
+            mapsUrl += where.coordinates[1] + ',' + where.coordinates[0];
+            if (!name) name = where.city + ', ' + where.country;
+            return <Link className="location" href={mapsUrl}>
+                <small><Icon name="map-marker"/> {name}</small>
+            </Link>;
+        }
+    },
     render: function() {
-        var Component = eventComponents[this.props.event.type];
-
-        return Component ? <Component event={this.props.event}/> : <div className="event">
+        var semantics = this.props.event.semantics;
+        var what = semantics.what;
+        return <div className="event">
             <header>
-                {this.props.event.description || this.props.event.type}
+                {this.getIcon(semantics)}{' '}
+                <Person {...semantics.who}/>{' '}
+                {semantics.verb}{' '}
+                {semantics.whom && <Person {...semantics.whom}/>}{' '}
+                <WhatList what={what}/>
+                <Complements complements={semantics.complements}/>
             </header>
-            {this.props.event.image ? <img src={this.props.event.image}/> : null}
+            <ul>
+                {what.map(function(what) {
+                    return <li className={what.type}>{this.render_what(what)}</li>;
+                }.bind(this))}
+            </ul>
+            {this.render_where(semantics.where)}
         </div>;
     }
 });
